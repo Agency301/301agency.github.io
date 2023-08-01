6b6bc83f07427582d2823514c03516160804af62
YoonahPark <cufft@naver.com>
2023-08-01 20:36:22 +0900

    Change filter.js

--- _layouts/filter.js
+++ _layouts/filter.js
@@ -1,15 +1,22 @@
 $("[data-tag]").click((e) => {
-  currentTag = e.target.dataset.tag;
-  filterByTagName(currentTag);
-})
-
-function filterByTagName(tagName) {
-  $('.hidden').removeClass('hidden');
-  $('.post-wrapper').each((index, elem) => {
-    if (!elem.hasAttribute(`data-${tagName}`)) {
-      $(elem).addClass('hidden');
-    }
+    const currentTag = e.target.dataset.tag;
+    filterByTagName(currentTag);
   });
-  $(`.tag`).removeClass('selected');
-  $(`.tag[data-tag=${tagName}]`).addClass('selected');
-}
+  
+  function filterByTagName(tagName) {
+    if (!tagName) {
+      // 태그가 선택되지 않았을 때 모든 게시물 보여주기
+      $('.post-wrapper').removeClass('hidden');
+    } else {
+      $('.hidden').removeClass('hidden');
+      $('.post-wrapper').each((index, elem) => {
+        if (!elem.hasAttribute(`data-${tagName}`)) {
+          $(elem).addClass('hidden');
+        }
+      });
+    }
+  
+    $(`.tag`).removeClass('selected');
+    $(`.tag[data-tag=${tagName}]`).addClass('selected');
+  }
+  
